88d35e7245eb5c19be6a5652adf3ce9ad9b7fd6b
alexfisher13 <alexfisher13@yandex.ru>
2021-08-27 10:27:07 +0300

    init

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+document.write("<nav class='pushy pushy-left' data-focus='#first-link'>\n" +
+    "    <div class='pushy-content'>\n" +
+    "        <ul>\n" +
+    "            <li class='pushy-link'><a href='/javajedi/pages/creation.html'>История создания</a></li>\n" +
+    "            <li class='pushy-link'><a href='/javajedi/pages/java1.html'>Java 1.0</a></li>\n" +
+    "        </ul>\n" +
+    "    </div>\n" +
+    "</nav>")
